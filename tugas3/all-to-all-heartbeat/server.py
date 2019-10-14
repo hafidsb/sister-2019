d850ed3c59c7afdf2ac94a31eee334f70bf87923
hafid <hfdbch@gmail.com>
2019-10-14 13:58:39 +0700

    bump

---
+++ tugas3/all-to-all-heartbeat/server.py
@@ -0,0 +1,25 @@
+from model import ServiceModel
+import Pyro4
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("Pyro4").setLevel(logging.DEBUG)
+
+Pyro4.config.COMMTIMEOUT = 3.0
+
+def start_server():
+    # name server harus di start dulu dengan  pyro4-ns -n localhost -p 7777
+    # gunakan URI untuk referensi name server yang akan digunakan
+    # untuk mengecek service apa yang ada di ns, gunakan pyro4-nsc -n localhost -p 7777 list
+
+    daemon = Pyro4.Daemon(host="localhost")
+    name_server = Pyro4.locateNS("localhost", 7777)
+    server = Pyro4.expose(ServiceModel)
+    server_uri = daemon.register(server)
+    print("server URI: ", server_uri)
+    name_server.register("myserver", server_uri)
+    daemon.requestLoop()
+
+
+if __name__ == '__main__':
+    start_server()
