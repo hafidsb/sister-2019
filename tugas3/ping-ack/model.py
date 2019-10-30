9f5e7caa3b684efc4a98050dfcb0ef4cfd0de9cd
hafid <hfdbch@gmail.com>
2019-10-30 09:14:01 +0700

    bad words

---
+++ tugas3/ping-ack/model.py
@@ -0,0 +1,72 @@
+import random
+import os
+import Pyro4.errors
+import time
+
+
+class ServiceModel(object):
+    def __init__(self):
+        self.lucky_number = 1
+
+    def set_lucky_number(self, number):
+        self.lucky_number = number
+
+    def get_lucky_number(self):
+        time.sleep(3)
+        return self.lucky_number
+
+    def connect(self, name="<empty>"):
+        # self.lucky_number = random.randint(1, 1000)
+        return "\nHello {}! Your lucky number is {}".format(name, self.lucky_number)
+
+    def create_file(self, file_name):
+        path = "file_storage/%d" % self.lucky_number
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+        f = open(path + '/' + file_name, 'w')
+        f.close()
+        return "File %s has been created successfully" % file_name
+
+    def read_file(self, file_name):
+        path = "file_storage/%d/%s" % (self.lucky_number, file_name)
+        try:
+            f = open(path, 'r')
+        except FileNotFoundError:
+            return False
+        return f.read()
+
+    def edit_file(self, file_name, content, mode):
+        path = "file_storage/%d/%s" % (self.lucky_number, file_name)
+        try:
+            f = open(path, mode)
+            if mode == 'a':
+                f.write(" " + content)
+            elif mode == 'w':
+                f.write(content)
+            f.close()
+        except FileNotFoundError:
+            return False
+        return self.read_file(file_name)
+
+    def list_file(self):
+        path = "file_storage/%d" % self.lucky_number
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+        return os.listdir(path)
+
+    def delete_file(self, file_name):
+        path = "file_storage/%d/%s" % (self.lucky_number, file_name)
+        try:
+            os.remove(path)
+            return "File %s has been deleted successfully" % file_name
+        except FileNotFoundError:
+            return "File %s is not found." % file_name
+
+
+if __name__ == '__main__':
+    k = ServiceModel()
+    print(k.connect("test from model"))
